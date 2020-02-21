aab5fe390c9a58a8b6f3a92fd6a1918cbcebc7ef
Salami Usman Olawale <usman@crenettechlabs.com>
2020-02-21 13:22:28 +0100

    install dotenv and connect mysql to node

--- server.js
+++ server.js
@@ -1,2 +1,19 @@
 const faker = require("faker");
 const mysql = require("mysql");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+connection.connect();
+
+connection.query("SELECT 1 + 1 AS solution", function(error, results, fields) {
+  if (error) throw error;
+  console.log("The solution is: ", results[0].solution);
+});
+
+connection.end();
