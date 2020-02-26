f43abc3e6a5659f0f3f80f89e6be300d184109e9
Salami Usman Olawale <usman@crenettechlabs.com>
2020-02-26 15:17:38 +0100

    create a query to insert 500 user to the database

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const faker = require("faker");
 const mysql = require("mysql");
-const sql = require("./sql");
+const schema = require("./schema");
 require("dotenv").config();
 
 const connection = mysql.createConnection({
@@ -13,8 +13,21 @@ const connection = mysql.createConnection({
 });
 connection.connect();
 
-connection.query(sql, function(error, results, fields) {
+/** Uncomment this to create user table
+ * 
+connection.query(schema, function(error, results) {
   if (error) throw error;
   console.log("Table created successfully ", results);
+});
+
+*/
+let data = [];
+for (let i = 0; i < 500; i++) {
+  data.push([faker.internet.email(), faker.date.past()]);
+}
+
+connection.query("INSERT INTO users (email, created_at) VALUES ? ", [data], (error, results) => {
+  if (error) throw error;
+  console.log("User inserted", results);
 });
 connection.end();
