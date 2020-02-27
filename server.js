8ff14c04322f51b023506a377183c6c4a396d046
devwalex <salamiusmanolawale@gmail.com>
2020-02-27 22:08:27 +0100

    insert user email into db

--- server.js
+++ server.js
@@ -2,11 +2,13 @@ const express = require("express");
 const faker = require("faker");
 const mysql = require("mysql");
 const schema = require("./schema");
+const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
-
 app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
 
 const connection = mysql.createConnection({
   host: process.env.DB_HOST,
@@ -35,12 +37,21 @@ connection.query(schema, function(error, results) {
 //   if (error) throw error;
 //   console.log("User inserted", results);
 // });
+
 app.get("/", (req, res) => {
   const query = "SELECT COUNT(*) AS count FROM users";
   connection.query(query, (error, results) => {
     if (error) throw error;
     const count = results[0].count;
     res.render("home", { count });
+  });
+});
+
+app.post("/join", (req, res) => {
+  const user = req.body;
+  connection.query("INSERT INTO users SET ?", user, error => {
+    if (error) throw error;
+    res.redirect("/");
   });
 });
 app.listen(8080, () => {
